7c9bb2c87a693754e1ae35fc34e2fea9cd31aa09
hasifahmed <hahmed@stuy.edu>
2019-04-05 14:00:46 -0400

    added streets

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 # SoftDev2 - pd8
 # P04
 
-import os
-
+import os, csv
 from flask import Flask, render_template, redirect, url_for, session, request, flash, get_flashed_messages
 
 
@@ -15,6 +14,12 @@ app.secret_key = os.urandom(32)
 @app.route('/')
 def index():
     return render_template('bubble.html')
+
+@app.route('/streets')
+def streets():
+    input_file = csv.reader(open("static/newdata.csv"))
+    next(input_file)
+    return render_template('streets.html', data = input_file)
 
 if __name__ == '__main__':
     app.debug = True
